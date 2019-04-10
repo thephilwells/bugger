f1e0dcaeca9d952805e975452fb5276a8d938b7e
Phil Wells <phil.wells@nytimes.com>
2019-04-10 13:32:25 -0400

    bugs -> heartthrobs

--- namer.js
+++ namer.js
@@ -1,11 +1,13 @@
-const adjectives = require('./adjectives').adjectives
-const bugs = require('./bugs').bugs
+const adjectives = require('./firsts').adjectives
+const bugs = require('./lasts').bugs
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-let adjectiveIndex = getRandomInt(adjectives.length)
-let bugIndex = getRandomInt(bugs.length)
+for (let i = 0; i < 10; i++) {
+  let adjectiveIndex = getRandomInt(adjectives.length)
+  let bugIndex = getRandomInt(bugs.length)
 
-console.log(adjectives[adjectiveIndex] + ' ' + bugs[bugIndex])
+  console.log(adjectives[adjectiveIndex] + ' ' + bugs[bugIndex])
+}
